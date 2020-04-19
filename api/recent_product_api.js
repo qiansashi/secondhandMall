17e7c7287ac1916e19cdad3dba363d788a5b5af7
qiansashi <44606577+qiansashi@users.noreply.github.com>
2020-04-19 21:10:17 +0800

    优化布局

--- api/recent_product_api.js
+++ api/recent_product_api.js
@@ -10,12 +10,13 @@ function getRecentProductUrl(status,listRows,pageNum){
 }
 
 function getRecentProductApi(apiJValue,listRows){
+  const images="/images";
   var productArray = new Array();
   for(var i=0;i<listRows;++i){
     productArray[i]={
     "id":apiJValue[i]["id"],
     "releaseName":apiJValue[i]["name"],
-    "DetailImgSrc":app.globalData.creeperUrl+apiJValue[i]["main_img_url"],
+    "DetailImgSrc":app.globalData.creeperUrl+images+apiJValue[i]["main_img_url"],
     "detailText":apiJValue[i]["summary"],
     "publisherName":apiJValue[i]["userinfo"]["nickname"],
     "publisherImgSrc":app.globalData.creeperUrl+apiJValue[i]["userinfo"]["img_url"]
